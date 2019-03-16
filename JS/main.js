59029ec4c0f50ab48445c224f0fac834c4373205
Anden Wieseler <35050901+JoeMcRainbow@users.noreply.github.com>
2019-03-15 23:12:22 -0500

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+/* 
+JavaScript rules:
+**const value cannot be changed later.
+**let (var name) can be changed later.
+**alert() will be shown to client. Use console.log()
+  for anything else.
+*/
+
+let age = 1;
+
+alert(age);
