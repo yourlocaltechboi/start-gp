008eab2bb7ea1b6f150fdfb22dcc457eb939c5d4
Anden Wieseler <35050901+JoeMcRainbow@users.noreply.github.com>
2019-03-31 13:33:43 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,6 @@ JavaScript rules:
   for anything else.
 */
 
-let warning = 'By accessing this webpage you hereby acknowledge that all Sioux Falls School District rules apply to your usage of this website, unauthorized usage of this website in any way is strictly prohibited, and will be reported to administration.';
+let warning = 'By accessing this webpage you hereby acknowledge that all Sioux Falls School District rules apply to your usage of this website, unauthorized usage of this website in any way is strictly prohibited, and will be reported to administration. District policies can be found at sf.k12.sd.us/our-district/district-policies';
 
 alert(warning);
