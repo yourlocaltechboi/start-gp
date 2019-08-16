115feaa3c641b3da6fa5f9a4a108c304f0bf48e9
Anden Wieseler <ttechno.alternate@hotmail.com>
2019-08-16 15:08:10 +0000

    edited the initial alert popup

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,6 @@ JavaScript rules:
   for anything else.
 */
 
-let warning = 'By accessing this webpage you hereby acknowledge that all Sioux Falls School District rules apply to your usage of this website, unauthorized usage of this website in any way is strictly prohibited, and will be reported to administration. District policies can be found at sf.k12.sd.us/our-district/district-policies . Additional legal information can be found in the legal pages, located at the bottom of the about page.';
+let warning = 'By accessing this webpage you hereby acknowledge that all Sioux Falls School District rules apply to your usage of this website, unauthorized usage of this website in any way is strictly prohibited, and will be reported to administration. District policies can be found at sf.k12.sd.us/our-district/district-policies . Additional legal information (Terms of service and SFSD AUP) can be found in the legal pages, located at the bottom of the about page.';
 
 alert(warning);
